647e20d675f308d9e4b1d53ea238bc7518e6d786
xqXQzzz1 <35725489+xqXQzzz1@users.noreply.github.com>
2021-07-03 12:49:54 +0800

    Update ShowIP.bat

--- Bat/ShowIP.bat
+++ Bat/ShowIP.bat
@@ -1,5 +1,6 @@
-@echo off
-for /f "tokens=4" %%a in ('route print^|findstr 0.0.0.0.*0.0.0.0') do (
+@echo off 
+setlocal enabledelayedexpansion
+for /f "tokens=4" %%a in ('route print^|findstr 0.0.0.0.*0.0.0.0^|findstr /V 198.18.0.1') do (
  set IP=%%a
 )
 echo %IP%
